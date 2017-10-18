a81099c91b7ea82e2d9d4bebe2d7da0cd1102a11
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-10-18 17:57:32 +0100

    Programmer: Use List<Programmer> for recommendations given and received

--- ProNet.Test/Unit/ProNet.cs
+++ ProNet.Test/Unit/ProNet.cs
@@ -11,39 +11,63 @@ namespace ProNet.Test.Unit
         [Test]
         public void Should_calculate_programmer_rank_for_two_programmers_with_a_single_recommendation()
         {
-            var programmer1 = new Programmer("Dave", new List<string>{"Bill"});
-            var programmer2 = new Programmer("Bill", new List<string>{"Dave"});
-            programmer1.UpdateRank(new Dictionary<string, Programmer>{{programmer1.Name, programmer2}, {programmer2.Name, programmer1}});
+            var programmer1 = new Programmer("Dave");
+            var programmer2 = new Programmer("Bill");
+
+            programmer1.Recommends(programmer2);
+            programmer1.IsRecommendedBy(programmer2);
+
+            programmer2.Recommends(programmer1);
+            programmer2.IsRecommendedBy(programmer1);
+
+            programmer1.UpdateRank();
+
             Assert.That(programmer1.Rank, Is.EqualTo(0.15m));
         }
 
         [Test]
         public void Should_calculate_the_number_of_recommendations_they_have()
         {
-            var programmer = new Programmer("Dave", new List<string> { "Bill" });
-            Assert.That(programmer.NumberOfRecommendations, Is.EqualTo(1));
+            var programmer1 = new Programmer("Dave");
+            var programmer2 = new Programmer("Bill");
+
+            programmer1.Recommends(programmer2);
+
+            Assert.That(programmer1.NumberOfRecommendations, Is.EqualTo(1));
         }
     }
 
     public class Programmer
     {
-        private readonly List<string> _recommendations;
         private readonly string _name;
+        private readonly List<Programmer> _recommends;
+        private readonly List<Programmer> _isRecommendedBy;
 
-        public Programmer(string name, List<string> recommendations)
+        public Programmer(string name)
         {
             _name = name;
-            _recommendations = recommendations;
+            _recommends = new List<Programmer>();
+            _isRecommendedBy = new List<Programmer>();
         }
 
-        public int NumberOfRecommendations => _recommendations.Count;
+        public int NumberOfRecommendations => _recommends.Count;
         public decimal Rank { get; private set; }
         public string Name => _name;
 
-        public void UpdateRank(Dictionary<string, Programmer> programmerRanks)
+        public void UpdateRank()
         {
             // (1 - d) + d(PR(T1)/C(T1)) + ... + d(PR(Tn)/C(Tn))
-            Rank = (1m - 0.85m) + (0.85m * (programmerRanks[_recommendations.First()].Rank/programmerRanks[_recommendations.First()].NumberOfRecommendations));
+            Rank = (1m - 0.85m) + (0.85m * (_isRecommendedBy.First().Rank/_isRecommendedBy.First()._recommends.Count));
+        }
+
+        public void Recommends(Programmer programmer)
+        {
+            _recommends.Add(programmer);
+        }
+
+        public void IsRecommendedBy(Programmer programmer)
+        {
+            _isRecommendedBy.Add(programmer);
         }
     }
 }
