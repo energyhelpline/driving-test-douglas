96cf5a204bb4041312d0c3a6937683e69b132264
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-10-25 12:31:29 +0100

    Move class to its own file

--- ProNet.Test/Unit/ProgrammerTests.cs
+++ ProNet.Test/Unit/ProgrammerTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace ProNet.Test.Unit
 {
@@ -50,45 +48,6 @@ namespace ProNet.Test.Unit
             programmer1.UpdateRank();
 
             Assert.That(programmer1.Rank, Is.EqualTo(0.7875m));
-        }
-    }
-
-    public class Programmer
-    {
-        private readonly List<Programmer> _recommendationsGiven;
-        private readonly List<Programmer> _recommendationsReceived;
-        private decimal _rank;
-
-        public Programmer()
-        {
-            _recommendationsGiven = new List<Programmer>();
-            _recommendationsReceived = new List<Programmer>();
-        }
-
-        public decimal Rank
-        {
-            get => _rank;
-            set => _rank = value;
-        }
-
-        public decimal ProgrammerRankShare => _rank / _recommendationsGiven.Count();
-
-        public void UpdateRank()
-        {
-            // (1 - d) + d(PR(T1)/C(T1)) + ... + d(PR(Tn)/C(Tn))
-            _rank = _recommendationsReceived
-                .Aggregate(1m - 0.85m, (current, programmer) => current + 0.85m * programmer.ProgrammerRankShare);
-        }
-
-        public void Recommends(Programmer programmer)
-        {
-            _recommendationsGiven.Add(programmer);
-            programmer.RecommendedBy(this);
-        }
-
-        private void RecommendedBy(Programmer programmer)
-        {
-            _recommendationsReceived.Add(programmer);
         }
     }
 }
---
+++ ProNet/Programmer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProNet
+{
+    public class Programmer
+    {
+        private readonly List<Programmer> _recommendationsGiven;
+        private readonly List<Programmer> _recommendationsReceived;
+        private decimal _rank;
+
+        public Programmer()
+        {
+            _recommendationsGiven = new List<Programmer>();
+            _recommendationsReceived = new List<Programmer>();
+        }
+
+        public decimal Rank
+        {
+            get => _rank;
+            set => _rank = value;
+        }
+
+        public decimal ProgrammerRankShare => _rank / _recommendationsGiven.Count();
+
+        public void UpdateRank()
+        {
+            // (1 - d) + d(PR(T1)/C(T1)) + ... + d(PR(Tn)/C(Tn))
+            _rank = _recommendationsReceived
+                .Aggregate(1m - 0.85m, (current, programmer) => current + 0.85m * programmer.ProgrammerRankShare);
+        }
+
+        public void Recommends(Programmer programmer)
+        {
+            _recommendationsGiven.Add(programmer);
+            programmer.RecommendedBy(this);
+        }
+
+        private void RecommendedBy(Programmer programmer)
+        {
+            _recommendationsReceived.Add(programmer);
+        }
+    }
+}
