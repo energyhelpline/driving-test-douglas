844b905d8f835e18906dce62f612b4e23fd29da4
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-10-25 12:29:08 +0100

    Removed unused using

--- ProNet.Test/Unit/ProgrammerTests.cs
+++ ProNet.Test/Unit/ProgrammerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -34,16 +33,36 @@ namespace ProNet.Test.Unit
 
             Assert.That(programmer2.ProgrammerRankShare, Is.EqualTo(0.2m));
         }
+
+        [Test]
+        public void Should_calculate_programmer_rank_for_a_programmer_with_multiple_recommendations()
+        {
+            var programmer1 = new Programmer();
+            var programmer2 = new Programmer();
+            var programmer3 = new Programmer();
+
+            programmer2.Rank = 0.5m;
+            programmer3.Rank = 0.25m;
+
+            programmer2.Recommends(programmer1);
+            programmer3.Recommends(programmer1);
+
+            programmer1.UpdateRank();
+
+            Assert.That(programmer1.Rank, Is.EqualTo(0.7875m));
+        }
     }
 
     public class Programmer
     {
         private readonly List<Programmer> _recommendationsGiven;
+        private readonly List<Programmer> _recommendationsReceived;
         private decimal _rank;
 
         public Programmer()
         {
             _recommendationsGiven = new List<Programmer>();
+            _recommendationsReceived = new List<Programmer>();
         }
 
         public decimal Rank
@@ -57,12 +76,19 @@ namespace ProNet.Test.Unit
         public void UpdateRank()
         {
             // (1 - d) + d(PR(T1)/C(T1)) + ... + d(PR(Tn)/C(Tn))
-            _rank = (1m - 0.85m) + (0.85m * (0));
+            _rank = _recommendationsReceived
+                .Aggregate(1m - 0.85m, (current, programmer) => current + 0.85m * programmer.ProgrammerRankShare);
         }
 
         public void Recommends(Programmer programmer)
         {
             _recommendationsGiven.Add(programmer);
+            programmer.RecommendedBy(this);
+        }
+
+        private void RecommendedBy(Programmer programmer)
+        {
+            _recommendationsReceived.Add(programmer);
         }
     }
 }
