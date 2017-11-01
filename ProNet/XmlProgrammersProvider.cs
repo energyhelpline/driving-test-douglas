7584cdc19145e725f12a2a376ced0de5dc1fb8a5
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-11-01 16:19:44 +0000

    Build list of programmers from xml

--- ProNet/XmlProgrammersProvider.cs
+++ ProNet/XmlProgrammersProvider.cs
@@ -1,14 +1,28 @@
-﻿namespace ProNet
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace ProNet
 {
     public class XmlProgrammersProvider : IProgrammersProvider
     {
         public IRankCalculator GetAll()
         {
-            return new RankedProgrammers(new []{new DummyProgrammer
-            {
-                Rank = 2.63m,
-                Name = "Nick"
-            }, new DummyProgrammer { Rank = 0.3m }, new DummyProgrammer { Rank = 0.07m }});
+            var xml = XElement.Parse(@"<?xml version=""1.0"" encoding=""utf-8"" ?>
+            <Network>
+                <Programmer name='Nick'></Programmer>
+                <Programmer name='Bill'></Programmer>
+                <Programmer name='Dave'></Programmer>
+            </Network>");
+
+            var programmers = xml
+                .Descendants("Programmer")
+                .Select(programmer => new DummyProgrammer{Name = programmer.Attribute("name").Value}).ToList();
+
+            programmers.First().Rank = 2.63m;
+            programmers.Skip(1).First().Rank = 0.3m;
+            programmers.Skip(2).First().Rank = 0.07m;
+
+            return new RankedProgrammers(programmers);
         }
     }
 }
