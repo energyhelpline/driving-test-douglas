2a3d0392f4dbdc4aee5016bc3aadd9ddca989124
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-11-01 15:45:29 +0000

    Add test for xml programmers provider

---
+++ ProNet.Test/Unit/XmlProgrammersProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ProNet.Test.Unit
+{
+    [TestFixture]
+    public class XmlProgrammersProviderTests
+    {
+        [Test]
+        public void Should_return_all_the_programmers_stored_as_xml()
+        {
+            var programmersProvider = new XmlProgrammersProvider();
+            var programmers = programmersProvider.GetAll();
+            var programmerA = programmers.First();
+            Assert.That(programmerA.Name, Is.EqualTo("Nick"));
+        }
+    }
+}
