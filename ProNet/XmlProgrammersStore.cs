ce5084bceb180955d0c442ea53fcff837bf69419
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-11-08 11:36:32 +0000

    Extract methods to reduce cyclomatic complexity

--- ProNet/XmlProgrammersStore.cs
+++ ProNet/XmlProgrammersStore.cs
@@ -19,20 +19,37 @@ namespace ProNet
 
         public IRankCalculator GetAll()
         {
-            var programmers = _xmlLoader.Load()
-                .Descendants("Programmer")
-                .Select(programmer => programmer.Attribute("name").Value )
-                .Select(programmer => new {
-                    Programmer = programmer,
-                    Recommendations = _xmlLoader.Load()
-                        .Descendants("Programmer")
-                        .Where(p => p.Attribute("name").Value == programmer)
-                        .Descendants("Recommendations")
-                        .Descendants("Recommendation")
-                        .Select(recommendation => recommendation.Value)})
-                .ToDictionary(tuple => tuple.Programmer, tuple => tuple.Recommendations);
+            var programmerNames = GetProgrammerNames();
+
+            var programmers = GetProgrammers(programmerNames);
 
             return _programmersBuilder.BuildProgrammers(programmers);
+        }
+
+        private Dictionary<string, IEnumerable<string>> GetProgrammers(IEnumerable<string> programmerNames)
+        {
+            return programmerNames
+                .Select(programmer => new {
+                    Programmer = programmer,
+                    Recommendations = GetRecommendations(programmer)})
+                .ToDictionary(tuple => tuple.Programmer, tuple => tuple.Recommendations);
+        }
+
+        private IEnumerable<string> GetProgrammerNames()
+        {
+            return _xmlLoader.Load()
+                .Descendants("Programmer")
+                .Select(programmer => programmer.Attribute("name").Value );
+        }
+
+        private IEnumerable<string> GetRecommendations(string programmer)
+        {
+            return _xmlLoader.Load()
+                .Descendants("Programmer")
+                .Where(p => p.Attribute("name").Value == programmer)
+                .Descendants("Recommendations")
+                .Descendants("Recommendation")
+                .Select(recommendation => recommendation.Value);
         }
     }
 }
