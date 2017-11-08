6eb0001243edd563974698f296db2ca4f46d3a7b
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-11-08 10:53:41 +0000

    Shortern identifier names in ranked programmers builder

--- ProNet/RankedProgrammersBuilder.cs
+++ ProNet/RankedProgrammersBuilder.cs
@@ -1,28 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace ProNet
 {
     public class RankedProgrammersBuilder : IProgrammersBuilder
     {
-        public IRankCalculator BuildProgrammers(IDictionary<string, IEnumerable<string>> programmers)
+        public IRankCalculator BuildProgrammers(IDictionary<string, IEnumerable<string>> programmerDictionary)
         {
-            var pageRankedProgrammers = new Dictionary<string, PageRankedProgrammer>();
+            var programmers = programmerDictionary
+                .ToDictionary(programmer => programmer.Key, programmer => new PageRankedProgrammer(programmer.Key));
 
-            foreach (var programmer in programmers)
+            foreach (var pageRankedProgrammer in programmers.Values)
             {
-                pageRankedProgrammers.Add(programmer.Key, new PageRankedProgrammer(programmer.Key));
-            }
-
-            foreach (var pageRankedProgrammer in pageRankedProgrammers.Values)
-            {
-                var recommendationNames = programmers[pageRankedProgrammer.Name];
-                foreach (var recommendationName in recommendationNames)
+                var recommendations = programmerDictionary[pageRankedProgrammer.Name];
+                foreach (var recommendation in recommendations)
                 {
-                    pageRankedProgrammer.Recommends(pageRankedProgrammers[recommendationName]);
+                    pageRankedProgrammer.Recommends(programmers[recommendation]);
                 }
             }
 
-            return new RankedProgrammers(pageRankedProgrammers.Values);
+            return new RankedProgrammers(programmers.Values);
         }
     }
 }
