1a9cdc3e84ec933d8bdf7fb02af801a099f80f34
Douglas Waugh <douglas.waugh@energyhelpline.com>
2017-11-08 11:31:51 +0000

    Extract methods to reduce cyclomatic complexity

--- ProNet/RankedProgrammersBuilder.cs
+++ ProNet/RankedProgrammersBuilder.cs
@@ -7,19 +7,33 @@ namespace ProNet
     {
         public IRankCalculator BuildProgrammers(IDictionary<string, IEnumerable<string>> programmerDictionary)
         {
-            var programmers = programmerDictionary
-                .ToDictionary(programmer => programmer.Key, programmer => new PageRankedProgrammer(programmer.Key));
+            var programmers = GetDictionaryOfPageRankedProgrammers(programmerDictionary);
 
-            foreach (var pageRankedProgrammer in programmers.Values)
-            {
-                var recommendations = programmerDictionary[pageRankedProgrammer.Name];
-                foreach (var recommendation in recommendations)
-                {
-                    pageRankedProgrammer.Recommends(programmers[recommendation]);
-                }
-            }
+            PopulateRecommendations(programmerDictionary, programmers);
 
             return new RankedProgrammers(programmers.Values);
+        }
+
+        private static Dictionary<string, PageRankedProgrammer> GetDictionaryOfPageRankedProgrammers(IDictionary<string, IEnumerable<string>> programmerDictionary)
+        {
+            return programmerDictionary
+                .ToDictionary(programmer => programmer.Key, programmer => new PageRankedProgrammer(programmer.Key));
+        }
+
+        private static void PopulateRecommendations(IDictionary<string, IEnumerable<string>> programmerDictionary, Dictionary<string, PageRankedProgrammer> programmers)
+        {
+            foreach (var pageRankedProgrammer in programmers.Values)
+            {
+                AddRecommendationsToProgrammer(pageRankedProgrammer, programmers, programmerDictionary[pageRankedProgrammer.Name]);
+            }
+        }
+
+        private static void AddRecommendationsToProgrammer(PageRankedProgrammer pageRankedProgrammer, Dictionary<string, PageRankedProgrammer> programmers, IEnumerable<string> recommendations)
+        {
+            foreach (var recommendation in recommendations)
+            {
+                pageRankedProgrammer.Recommends(programmers[recommendation]);
+            }
         }
     }
 }
